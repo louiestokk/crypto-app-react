54148fcb90fdf5ac92f95325af37d18769ee59aa
Your Name <louiestokk>
2022-01-06 19:42:26 +0100

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import TopSection from "../components/TopSection";
-
+import SingelCoin from "../components/SingelCoin";
+import CoinGecko from "../components/CoinGecko";
 function Home() {
-  return <div className="man-container"></div>;
+  return (
+    <div className="man-container">
+      <CoinGecko />
+      <SingelCoin />
+    </div>
+  );
 }
 
 export default Home;
